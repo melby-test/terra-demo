1621dfd70f1d0068584ec412757909c6e0c305da
MelbyOommen <melbyoommen@gmail.com>
2024-11-23 02:07:15 +0530

    first

---
+++ linux-instance.tf
@@ -0,0 +1,42 @@
+resource "azurerm_network_interface" "terra-demo" {
+  name                = "terra-demo-nic"
+  location            = azurerm_resource_group.terra-demo.location
+  resource_group_name = azurerm_resource_group.terra-demo.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.terra-demo.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id=azurerm_public_ip.terra-demo.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "terra-demo" {
+  name                = "terra-demo-machine01"
+  resource_group_name = azurerm_resource_group.terra-demo.name
+  location            = azurerm_resource_group.terra-demo.location
+  size                = "Standard_L8as_v3 "
+  admin_username      = "adminuser"
+  admin_password      ="admin@123"
+  disable_password_authentication= "false"
+
+  network_interface_ids = [
+    azurerm_network_interface.terra-demo.id,
+  ]
+  #   admin_ssh_key {
+  #  username   = "adminuser"
+  #  admin_pa
+  #  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,23 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  ## resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  subscription_id = "126411d9-3da5-4136-8032-8b257a0dd75d"
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "terra-demo" {
+  name     = "terra-demo-rg"
+  location = "East Us"
+}
